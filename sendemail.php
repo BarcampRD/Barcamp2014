becb42263e48adb52f53c6e848d44330fb79d162
Diego Sanchez <diegosanchez93@outlook.com>
2014-09-27 17:24:07 -0400

    Subiendo los archivos.

---
+++ sendemail.php
@@ -0,0 +1,21 @@
+<?php
+	header('Content-type: application/json');
+	$status = array(
+		'type'=>'success',
+		'message'=>'Gracias por contactarnos. Estaremos respondiendo tu inquietud tan proto sea posible.'
+	);
+
+    $name = @trim(stripslashes($_POST['name'])); 
+    $email = @trim(stripslashes($_POST['email'])); 
+    $subject = "Barcamp Santiago"; 
+    $message = @trim(stripslashes($_POST['message'])); 
+
+    $email_from = $email;
+    $email_to = 'barcamprd@gmail.com';//replace with your email
+
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+    echo json_encode($status);
+    die;
