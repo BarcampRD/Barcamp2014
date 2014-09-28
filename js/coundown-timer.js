73d7bc1cc3388ad6bfbffe818d6493527db2cef8
Diego Sanchez <diegosanchez93@outlook.com>
2014-09-28 11:05:36 -0400

    Plantilla modificada

--- js/coundown-timer.js
+++ js/coundown-timer.js
@@ -38,10 +38,10 @@
 			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
 			
 			//conditional Ss
-			if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
-			if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
-			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
-			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
+			if (days == 1) { thisEl.find(".timeRefDays").text("d&iacute;as"); } else { thisEl.find(".timeRefDays").text("d&iactute;as"); }
+			if (hours == 1) { thisEl.find(".timeRefHours").text("hora"); } else { thisEl.find(".timeRefHours").text("horas"); }
+			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minuto"); } else { thisEl.find(".timeRefMinutes").text("minutos"); }
+			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("segundo"); } else { thisEl.find(".timeRefSeconds").text("segundos"); }
 			
 			//logic for the two_digits ON setting
 			if(settings['format'] == "on") {
